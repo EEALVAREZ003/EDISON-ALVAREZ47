7e6ee128a6f6fcf630e9c59387b3b638cb69d591
edisson <165519410+EEALVAREZ003@users.noreply.github.com>
2024-07-22 23:04:17 -0500

    configuración usuario y contraseña

--- CLASE 05-06/js/logica.js
+++ CLASE 05-06/js/logica.js
@@ -1,3 +1,29 @@
-console.log('logica conectada')
+console.log('logica conectada');
+
+const usuario = 'admin'
+const clave = '1234'
+
+
+
+function pruebaboton(){
+
+    let user = document.getElementById('user').value
+    let pw = document.getElementById('pw').value
+
+    if (usuario==user && clave==pw) {
+        console.log('ingreso correcto');
+     } else {
+            console.log("error de contraseña");
+        } 
+    
+
+
+    console.log ("usuario ${user} clave ${pw}")
+
+
+}
+
+
+
 
 

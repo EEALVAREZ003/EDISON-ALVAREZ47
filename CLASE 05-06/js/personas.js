77ac76532c83b512b2b8c19bc3f7b0a6a56e2a68
edisson <165519410+EEALVAREZ003@users.noreply.github.com>
2024-08-01 23:36:03 -0500

    JSON

---
+++ CLASE 05-06/js/personas.js
@@ -0,0 +1,81 @@
+//vector
+let dataPersonas = [
+    { id: 1, nombre: 'oscar', edad: 20 },
+    { id: 2, nombre: 'juan', edad: 39 },
+    { id: 3, nombre: 'pedro', edad: 25 }
+]
+
+//JSON
+let dataPersonasJSON = [
+    {
+        'id': 1,
+        'nombre': 'oscar',
+        'edad': 20
+        'deportes': [
+            {
+                'deporte': 'tennis',
+                'dias': ['lunes', 'miercoles'],
+                'tiempo': 60
+            },
+            {
+                'deporte': 'futbol',
+                'dias': ['martes','jueves', 'sabado'],
+                'tiempo': 90
+            }
+        ]
+    }
+    
+]
+
+let nombrePlatoJSON = [
+    {
+        "nombre plato": "mojarra frita",
+        "precio": 30000,
+        "chef": "carlos",
+        "ingredientes": [
+            {
+                "nombre": "mojarra",
+                "cantidad": 1,
+                "precio": 5000
+            },
+            {
+                "nombre": "arroz",
+                "cantidad": "1 libra",
+                "precio": 2000
+            },
+            {
+                "nombre": "patacon",
+                "cantidad": 3,
+                "precio": 1500
+            },
+            {
+                "nombre": "papas",
+                "cantidad": "2 libras"
+                "precio": 4000
+            },
+        ],
+        "acompañantes": [
+            {
+                "nombre": "mojarra",
+                "cantidad": 1,
+                "precio": 5000
+            },
+            {
+                "nombre": "arroz",
+                "cantidad": "1 libra",
+                "precio": 2000
+            },
+            {
+                "nombre": "patacon",
+                "cantidad": 3,
+                "precio": 1500
+            },
+            {
+                "nombre": "papas",
+                "cantidad": "2 libras"
+                "precio": 4000
+            },
+        ]
+    }
+    
+]

1151d7c5654dbcfaf33945243ebd4daf991bfb09
edisson <165519410+EEALVAREZ003@users.noreply.github.com>
2024-08-07 22:38:13 -0500

    funcion some clase del 5 de julio

--- CLASE 05-06/js/logica.js
+++ CLASE 05-06/js/logica.js
@@ -3,26 +3,49 @@ console.log('logica conectada');
 const usuario = 'admin'
 
 const clave = '1234'
+let contador = 0
 
+let usuarios=[
+{"usuario":"edisson", "clave": "1111"},
+{"usuario":"admin", "clave": "2222"},
+{"usuario":"edisson", "clave": "3333"},
+{"usuario":"admin", "clave": "4444"}
+]
 
-
-function pruebaboton() {
-
+function pruebaboton(){
     let user = document.getElementById('user').value.toLocaleUpperCase()
     let pw = document.getElementById('pw').value
+    let usuariologueado= usuarios.some(usuario=> usuario.usuario.toLocaleUpperCase()==user && usuario.clave==pw)
 
-
-    if (usuario.toLocaleUpperCase() == user && clave == pw) {
-        //en esta parte el usuario se encuentra logueado
+    if (usuariologueado){
         setUser()
-        window.open('localstorage.html')
-    } else {
-
-        bloquearcuenta()
+      window.open('localstorage.html')
+    } else {bloquearcuenta()
         console.log('error de contraseña');
+
     }
 }
+
+
+
+//function pruebaboton() {
+
+  //  let user = document.getElementById('user').value.toLocaleUpperCase()
+    //let pw = document.getElementById('pw').value
+
+
+    //if (usuario.toLocaleUpperCase() == user && clave == pw) {
+        //en esta parte el usuario se encuentra logueado
+        //setUser()
+      //  window.open('localstorage.html')
+    //} else {
+
+      //  bloquearcuenta()
+    //    console.log('error de contraseña');
+  //  }
+//}
 //bloquear el acceso del usuario
+
 function bloquearcuenta(){
     
     

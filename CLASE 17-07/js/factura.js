27cf4b7559cca53631dcd842698b20a13030136b
edisson <165519410+EEALVAREZ003@users.noreply.github.com>
2024-08-21 23:52:38 -0500

    minuto 02:03:00 clase del 17 de julio

--- CLASE 17-07/js/factura.js
+++ CLASE 17-07/js/factura.js
@@ -1,3 +1,38 @@
 let usuarios=JSON.parse(localStorage.getItem('usuarios'))|| [];
 
-console.log(usuarios);
+let dataServicios = [
+    { id: 1, nombre: 'lavado general', precio: 20000 },
+    { id: 2, nombre: 'polichada', precio: 50000 },
+    { id: 3, nombre: 'lavado a presion', precio: 40000 },
+    { id: 4, nombre: 'servicio completo', precio: 70000 },
+    { id: 5, nombre: 'cambio de aceite', precio: 25000 }
+]
+
+const selecTipoCliente=document.getElementById('tipoCliente')
+usuarios.forEach(element => {
+    const option= document.createElement('option')
+    option.value= element.tipoCliente
+    option.text= `${element.nombres} ${element.apellidos}`
+    selecTipoCliente.appendChild(option)
+});
+
+const selectServicios=document.getElementById('servicios')
+dataServicios.forEach(element => {
+    const option= document.createElement('option')
+    option.value= element.precio
+    option.text= `${element.nombre}`
+    selectServicios.appendChild(option)
+});
+
+function calcularTotal(){
+    const valorServicio = parseInt(document.getElementById('servicios').value)
+    const tipoCliente = document.getElementById('tipoCliente').value
+    const mecanico = parseInt(document.getElementById('mecanico').value)
+    const repuesto = parseInt(document.getElementById('repuesto').value)
+
+    const total= valorServicio+mecanico+repuesto
+
+    console.log(total)
+   
+}
+
